5c4d7fd1f9da1436aafb056bfc2a9171e7cf96e8
Ali Haider <alidevofficial@gmail.com>
2024-10-19 15:11:50 +0500

    make changes

--- Content-Project/src/App.jsx
+++ Content-Project/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <h1>First Project</h1>
+      <Navbar />
     </>
   );
 }
